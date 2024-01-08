931538e71d273e1b64c9865731dfd18bbea56e50
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-08 13:35:26 +0300

    Implementation of the TestGithubOrgClient.test_public_repos to unit-test GithubOrgClient.public_repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -62,43 +62,40 @@ class TestGithubOrgClient(unittest.TestCase):
             test_client._public_repos_url, expected_dict["repos_url"]
         )
 
-    @parameterized.expand(
-        [
-            (
-                "google", {
-                    "repos_url": "https://api.github.com/orgs/google/repos"
-                }
-            ),
-            ("abc", {"repos_url": "https://api.github.com/orgs/abc/repos"}),
-        ]
+    @patch(
+        "client.GithubOrgClient._public_repos_url", new_callable=PropertyMock
     )
-    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
-    @patch("client.get_json", return_value=[
-        {"name": "repo1"}, {"name": "repo2"}
-        ]
-    )
+    @patch("client.get_json")
     def test_public_repos(
         self,
-        org_name,
-        expected_dict,
-        mock_org,
-        mock_get_json
+        mock_get_json,
+        mock_public_repos_url
     ):
         """
         Tests that `GithubOrgClient.public_repos` returns the correct value.
 
         Args:
-            org_name (str): The name of the Github organization to test.
-            expected_dict (dict): The expected dictionary containing
-            the repos_url.
+            mock_get_json (MagicMock): A mock of `get_json`.
+            mock_public_repos_url (PropertyMock): A mock of
+            `_public_repos_url`.
 
         Asserts that the output of `GithubOrgClient.public_repos` is equal
         to the expected list of repo names.
         """
-        mock_org.return_value = expected_dict
-        test_client = GithubOrgClient(org_name)
-        self.assertEqual(test_client.public_repos, ["repo1", "repo2"])
-        mock_get_json.assert_called_once_with(expected_dict["repos_url"])
+        test_org_name = "google"
+        expected_repos = ["repo1", "repo2"]
+        mock_get_json.return_value = [
+            {"name": repo_name} for repo_name in expected_repos
+        ]
+        mock_public_repos_url.return_value = (
+            f"https://api.github.com/orgs/{test_org_name}/repos"
+        )
+
+        test_client = GithubOrgClient(test_org_name)
+        self.assertEqual(test_client.public_repos, expected_repos)
+        mock_get_json.assert_called_once_with(
+            mock_public_repos_url.return_value
+        )
 
     @parameterized.expand(
         [
